cd10d9f5cf65d4b7fec7a933f1f71d376e6b1b32
Joey F. Poon <joey@joeypoon.com>
2016-09-25 17:25:32 -0500

    Set up base backend utils.

--- app/utils/backendUtils.js
+++ app/utils/backendUtils.js
@@ -1,5 +1,29 @@
+import { logEvent } from './facebookUtils';
 import { postPlayersUrl } from '../constants/urls';
 
+function get (url) {
+    return fetch(url).catch(catchException);
+}
+
+function post (url, body) {
+    return fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body)
+    }).catch(catchException);
+}
+
+function put (url, body) {
+    return fetch(url, {
+        method: 'PUT',
+        body: JSON.stringify(body)
+    }).catch(catchException);
+}
+
+function catchException (err) {
+    // error modal
+    logEvent('error', null, err);
+}
+
 export function postPlayers (body) {
-    fetch(postPlayersUrl, { method: 'POST', body: JSON.stringify(body) });
+    post(postPlayersUrl, body);
 }
