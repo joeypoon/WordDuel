da7949a23ea35b8140e6dad8088d76aee8b82d6a
Joey F. Poon <joey@joeypoon.com>
2016-10-10 20:18:54 -0500

    Working websockets.

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import io from 'socket.io-client/socket.io';
 
 import Main from './Main';
 import { store } from '../store';
 import { requestData } from '../utils/facebookUtils';
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.socket = io('http://localhost:3000', { jsonp: false });
+        this.socket.on('connect', () => {
+          console.log('connected!');
+        });
+    }
+
     componentDidMount() {
         requestData();
     }
