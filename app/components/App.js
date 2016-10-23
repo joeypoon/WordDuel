c26cb4d15fa5a92d4ebb37600fce6b149e6c2721
Joey F. Poon <joey@joeypoon.com>
2016-10-22 19:14:31 -0500

    remove unused imports

--- app/components/App.js
+++ app/components/App.js
@@ -4,8 +4,6 @@ import { Provider } from 'react-redux';
 import Main from './Main';
 import { store } from '../store';
 import { requestData } from '../utils/facebookUtils';
-import { socket } from '../socket';
-import { events } from '../constants';
 
 import '../socketListeners';
 
@@ -15,7 +13,7 @@ export default class App extends Component {
     }
 
     render() {
-        return <Provider store = { store }>
+        return <Provider store={ store }>
             <Main />
         </Provider>;
     }
