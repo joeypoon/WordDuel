2a6b01869192cb0db3a9f36c74c2cd55b0688086
Joey F. Poon <joey@joeypoon.com>
2016-10-27 19:23:49 -0500

    Restrict opponent disconnect to duel.

--- app/socketHandlers/matches.js
+++ app/socketHandlers/matches.js
@@ -6,6 +6,9 @@ import {
 import { modalTypes } from '../constants';
 
 export function onMatchDisconnect() {
-    store.dispatch(setModalType(modalTypes.playerDisconnect));
-    store.dispatch(setModalVisible(true));
+    const opponentSocket = store.getState().opponent.get('socket');
+    if (opponentSocket) {
+        store.dispatch(setModalType(modalTypes.playerDisconnect));
+        store.dispatch(setModalVisible(true));
+    }
 }
