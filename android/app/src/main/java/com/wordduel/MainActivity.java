f693a38310d4c831cc4ddfd96282018ed8cfc841
Joey F. Poon <joey@kaijudev.com>
2017-01-03 22:36:20 -0600

    Working on physical android device.

--- android/app/src/main/java/com/wordduel/MainActivity.java
+++ android/app/src/main/java/com/wordduel/MainActivity.java
@@ -1,6 +1,7 @@
 package com.wordduel;
 
 import com.facebook.react.ReactActivity;
+import android.content.Intent;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +12,11 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "WordDuel";
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    super.onActivityResult(requestCode, resultCode, data);
+	    MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
     }
 }
